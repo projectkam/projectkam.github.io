3fe3286a967225f66b2d3312e3c53ff72050b182
ilham zunaidy <ilhamzunaidy4@gmail.com>
2023-04-21 00:46:29 +0700

    update

---
+++ assets/js/famdata.js
@@ -0,0 +1,9 @@
+function data(famType) {
+    if (famType == 'datafam1') {
+        var datafam1 = fam1()
+        return datafam1 
+    } else if (famType == 'datafam2') {
+        var datafam2 = fam2()
+        return datafam2
+    }    
+}
